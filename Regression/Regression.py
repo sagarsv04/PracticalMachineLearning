2ea3a5d31bc594f921816672520309abc324bd0b
sagarsv <scoutsv.sv@gmail.com>
2018-02-08 00:37:58 +0530

    defined label from features

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -1,5 +1,24 @@
 
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
+
+impdf = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+
+impdf['HL_PCT'] = (impdf['Adj. High'] - impdf['Adj. Close']) / impdf['Adj. Close'] * 100.0
+
+impdf['PCT_Change'] = (impdf['Adj. Close'] - impdf['Adj. Open']) / impdf['Adj. Open'] * 100.0
+
+impdf = impdf[['Adj. Close','HL_PCT','PCT_Change','Adj. Volume']]
+
+forecastCol = 'Adj. Close'
+
+impdf.fillna(-99999, inplace = True)
+
+forecastOut = int(math.ceil(0.01*len(impdf)))
+
+impdf['label'] = impdf[forecastCol].shift(-forecastOut)
+
+impdf.dropna(inplace=True)
