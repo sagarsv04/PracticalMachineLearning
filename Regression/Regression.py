50cf3e01b0802ce2daf417b77631b006b96d8bbd
sagarsv <scoutsv.sv@gmail.com>
2018-02-09 00:31:17 +0530

    issue in pandas series plot()

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -5,8 +5,13 @@ import math
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
 
-df = quandl.get('WIKI/GOOGL')
+style.use('ggplot')
+
+df = quandl.get('WIKI/GOOGL', api_key='6yDydmxh-cSGy9vzbs74')
 
 impdf = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
 
@@ -24,19 +29,23 @@ forecastOut = int(math.ceil(0.01*len(impdf)))
 
 impdf['label'] = impdf[forecastCol].shift(-forecastOut)
 
-impdf.dropna(inplace=True)
-
 X = np.array(impdf.drop(['label'],1))
-y = np.array(impdf['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecastOut]
+type(X)
 
+XLately = X[-forecastOut:]
+
+impdf.dropna(inplace=True)
+y = np.array(impdf['label'])
+type(y)
 # X = X[: -forecastOut + 1]
-
+# find min length and use that much data
+n = min(len(X), len(y))
 # impdf.dropna(inplace=True)
 y = np.array(impdf['label'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X[:n], y[:n], test_size = 0.2)
 
 clf = LinearRegression()
 # switch algo
@@ -45,3 +54,23 @@ clf = LinearRegression()
 clf.fit(X_train, y_train)
 
 accu = clf.score(X_train,y_train)
+
+forecastSet = clf.predict(XLately)
+impdf['Forecast'] = np.nan
+
+lastDate = df.iloc[-1].name
+lastUnix = lastDate.timestamp()
+oneDay = 86400
+nextUnix = lastUnix + oneDay
+
+for i in forecastSet:
+    nextDate = datetime.datetime.fromtimestamp(nextUnix)
+    nextUnix += oneDay
+    impdf.loc[nextDate] = [np.nan for _ in range(len(impdf.columns)-1)] + [i]
+
+
+impdf['Adj. Close'].plot()
+impdf['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
