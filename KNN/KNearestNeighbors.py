2bc50e6f511e8de97fea3e9010bfc91267a2d9a6
sagarsv <scoutsv.sv@gmail.com>
2018-02-14 00:23:41 +0530

    own knn done on same data

--- KNN/KNearestNeighbors.py
+++ KNN/KNearestNeighbors.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 from sklearn import preprocessing, cross_validation, neighbors
-import os
+import os, random
 from collections import Counter
 from math import sqrt
 import warnings
@@ -12,12 +12,17 @@ import warnings
 # os.chdir(os.getcwd()+'\\KNN')
 
 # Basic KNN Using Breast Cancer Wisconsin data from UCI
-def KNNUsingSKLearn():
-
+def ReadData():
     df = pd.read_csv('breastCancerWisconsin.data')
     df.replace('?',-99999,inplace=True)
     # remove coloum id as its useless including it can reduce your accuracy significantly
     df.drop('id',1,inplace=True)
+    return df;
+
+
+def KNNUsingSKLearn():
+
+    df = ReadData()
 
     # features
     X = np.array(df.drop(['class'],1))
@@ -59,8 +64,9 @@ def kNNAlgo(data, predict, k=3):
     # return first k values
     votes = [i[1] for i in sorted(distance)[:k]]
     voteResult = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return voteResult
+    return voteResult, confidence
 
 
 def PlotData():
@@ -68,22 +74,48 @@ def PlotData():
     # plotTwo = [5,7]
     # euclideanDist = sqrt((plotOne[0] - plotTwo[0])**2 + (plotOne[1] - plotTwo[1])**2)
     # print(euclideanDist)
-    style.use('fivethirtyeight')
 
+    # Test with example datasel #
     # two classes and there features
-    dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+    # dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
+    # newFeature = [5,7]
+    # [[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]]for i in dataset]
+    # result = kNNAlgo(dataset,newFeature,k=3)
+    # plt.scatter(newFeature[0],newFeature[1],s=100,color=result)
 
-    newFeature = [5,7]
-    [[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]]for i in dataset]
+    df = ReadData()
+    fullData = df.astype(float).values.tolist()
+    random.shuffle(fullData)
 
-    result = kNNAlgo(dataset,newFeature,k=3)
+    testSize = 0.2
+    trainSet = {2:[],4:[]}
+    testSet = {2:[],4:[]}
+    # len(fullData[:-int(testSize*len(fullData))]) + len(fullData[-int(testSize*len(fullData)):])
+    trainData = fullData[:-int(testSize*len(fullData))]
+    testData = fullData[-int(testSize*len(fullData)):]
 
-    plt.scatter(newFeature[0],newFeature[1],s=100,color=result)
+    for i in trainData:
+        # i=trainData[0]
+        trainSet[i[-1]].append(i[:-1])
 
-    plt.show()
+    for i in testData:
+        testSet[i[-1]].append(i[:-1])
 
+    correct = 0
+    total = 0
 
-    print(result)
+    for group in testSet:
+        # group = 2
+        for data in testSet[group]:
+            # data = testSet[group][0]
+            vote, confidence = kNNAlgo(trainSet,data,k=5)
+            if group == vote:
+                correct += 1
+            total += 1
+    # style.use('fivethirtyeight')
+    # plt.show()
+    # print(result)
+    print("Accuracy:", correct/total)
 
     return 0;
 
