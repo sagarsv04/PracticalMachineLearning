09ce418ba0c43baf0dfa1e5cb24cddf9dadbccc1
sagarsv04 <scoutsv.sv@gmail.com>
2018-03-06 00:15:55 +0530

    added class with fit method - MeanShift from strach

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -299,6 +299,87 @@ def MeanShiftTitanicDataset():
     return 0
 
 
+class Mean_Shift(object):
+    """docstring for Mean_Shift."""
+    def __init__(self, bandwidth=4):
+        self.bandwidth = bandwidth
+
+    def fit(self, data):
+        centroids = {}
+
+        for i in range(len(data)):
+            # i = 0
+            centroids[i] = data[i]
+
+        while True:
+            new_centroids = []
+            for i in centroids:
+                # i = 0
+                in_bandwidth = []
+                centroid = centroids[i]
+                for featureset in data:
+                    # featureset = data[6]
+                    # calculate euclidean norm
+                    if np.linalg.norm(featureset-centroid) < self.bandwidth:
+                        in_bandwidth.append(featureset)
+
+                new_centroid = np.average(in_bandwidth,axis=0)
+                new_centroids.append(tuple(new_centroid))
+
+            uniques = sorted(list(set(new_centroids)))
+
+            prev_centroids = dict(centroids)
+
+            #  now take only unique centroids
+            centroids = {}
+            for i in range(len(uniques)):
+                # i = 0
+                centroids[i] = np.array(uniques[i])
+
+            optimized = True
+
+            for i in centroids:
+                # i = 0
+                if not np.array_equal(centroids[i], prev_centroids[i]):
+                    optimized = False
+                if not optimized:
+                    break
+
+            if optimized:
+                break
+
+        # save the centroids on train dataset to use with test data
+        self.centroids = centroids
+
+    def predict(self,data):
+        pass
+
+
+def MeanShiftFromStrach():
+
+    data = GetData()
+    extra_data = np.array([[8,2],[10,2],[9,3]])
+    data = np.concatenate((data, extra_data))
+    colors = 10*["g","r","c","b","k"]
+    # plt.scatter(data[:, 0],data[:, 1], s=150)
+
+    clf = Mean_Shift()
+    clf.fit(data)
+
+    centroids = clf.centroids
+
+    plt.scatter(data[:,0], data[:,1], s=150)
+
+    for c in centroids:
+        plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150)
+
+    plt.show()
+
+
+    return 0
+
+
+
 def main():
     # KMean is a flat clustering methodology
     # ClusterUsingSKLearn()
@@ -308,7 +389,8 @@ def main():
 
     # MeanShift is a hierarchical clustering methodology
     # MeanShiftUsingSKLearn()
-    MeanShiftTitanicDataset()
+    # MeanShiftTitanicDataset()
+    MeanShiftFromStrach()
 
     return 0
 
