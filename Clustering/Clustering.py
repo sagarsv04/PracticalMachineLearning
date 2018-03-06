ca85f37f2531e524e69c6393307001da772ca1f2
sagarsv04 <scoutsv.sv@gmail.com>
2018-03-07 00:32:26 +0530

    dynamically calculating radius on data

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -1,4 +1,4 @@
-
+radius
 import pandas as pd
 import numpy as np
 import os, random
@@ -9,6 +9,7 @@ from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
 from matplotlib import style
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.datasets.samples_generator import make_blobs
 
 style.use('ggplot')
 
@@ -300,16 +301,29 @@ def MeanShiftTitanicDataset():
 
 
 class Mean_Shift(object):
-    """docstring for Mean_Shift."""
-    def __init__(self, bandwidth=4):
-        self.bandwidth = bandwidth
+    # radius_norm_step we wanna have a lot of steps in the radius like lot of bandwidths
+    def __init__(self, radius = None, radius_norm_step = 100):
+        self.radius = radius
+        self.radius_norm_step = radius_norm_step
 
     def fit(self, data):
+
+        if self.radius == None:
+            # find centroid of all the data
+            all_data_centroid = np.average(data, axis = 0)
+            # get magnitude from the origin
+            all_data_norm = np.linalg.norm(all_data_centroid)
+            # we will use above information to define a radius
+            self.radius = all_data_norm / self.radius_norm_step
+            print("Radius:",self.radius)
+
         centroids = {}
 
         for i in range(len(data)):
             # i = 0
             centroids[i] = data[i]
+
+        weights = [i for i in range(self.radius_norm_step)][::-1]
 
         while True:
             new_centroids = []
@@ -317,16 +331,46 @@ class Mean_Shift(object):
                 # i = 0
                 in_bandwidth = []
                 centroid = centroids[i]
+
                 for featureset in data:
                     # featureset = data[6]
-                    # calculate euclidean norm
-                    if np.linalg.norm(featureset-centroid) < self.bandwidth:
-                        in_bandwidth.append(featureset)
+
+                    # # calculate euclidean norm
+                    # if np.linalg.norm(featureset-centroid) < self.radius:
+                    #     in_bandwidth.append(featureset)
+
+                    distance = np.linalg.norm(featureset-centroid)
+                    if distance == 0:
+                        distance = 0.00000000001
+                    weight_index = int(distance/self.radius)
+                    if weight_index > self.radius_norm_step-1:
+                        weight_index = self.radius_norm_step-1
+
+                    to_add = (weights[weight_index]**2)*[featureset]
+                    in_bandwidth +=to_add
 
                 new_centroid = np.average(in_bandwidth,axis=0)
                 new_centroids.append(tuple(new_centroid))
 
             uniques = sorted(list(set(new_centroids)))
+
+            to_pop = []
+
+            for i in uniques:
+                for ii in [i for i in uniques]:
+                    if i == ii:
+                        pass
+                    elif np.linalg.norm(np.array(i)-np.array(ii)) <= self.radius:
+                        #print(np.array(i), np.array(ii))
+                        to_pop.append(ii)
+                        break
+
+            for i in to_pop:
+                try:
+                    uniques.remove(i)
+                except:
+                    pass
+
 
             prev_centroids = dict(centroids)
 
@@ -342,17 +386,34 @@ class Mean_Shift(object):
                 # i = 0
                 if not np.array_equal(centroids[i], prev_centroids[i]):
                     optimized = False
-                if not optimized:
-                    break
+
 
             if optimized:
                 break
 
         # save the centroids on train dataset to use with test data
         self.centroids = centroids
+        self.classifications = {}
+
+        for i in range(len(self.centroids)):
+            self.classifications[i] = []
+
+        for featureset in data:
+            #compare distance to either centroid
+            distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+            #print(distances)
+            classification = (distances.index(min(distances)))
+
+            # featureset that belongs to that cluster
+            self.classifications[classification].append(featureset)
+
+
 
     def predict(self,data):
-        pass
+        #compare distance to either centroid
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = (distances.index(min(distances)))
+        return classification
 
 
 def MeanShiftFromStrach():
@@ -360,18 +421,27 @@ def MeanShiftFromStrach():
     data = GetData()
     extra_data = np.array([[8,2],[10,2],[9,3]])
     data = np.concatenate((data, extra_data))
+
+    X, y = make_blobs(n_samples=25, centers=3, n_features=2)
     colors = 10*["g","r","c","b","k"]
     # plt.scatter(data[:, 0],data[:, 1], s=150)
 
     clf = Mean_Shift()
-    clf.fit(data)
+    # clf.fit(data)
+    clf.fit(X)
 
     centroids = clf.centroids
 
-    plt.scatter(data[:,0], data[:,1], s=150)
+    # plt.scatter(data[:,0], data[:,1], s=150)
+
+    for classification in clf.classifications:
+        color = colors[classification]
+        for featureset in clf.classifications[classification]:
+            plt.scatter(featureset[0],featureset[1], marker = "x", color=color, s=150, linewidths = 5, zorder = 10)
+
 
     for c in centroids:
-        plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150)
+        plt.scatter(centroids[c][0], centroids[c][1], color='k', marker='*', s=150, linewidths = 5)
 
     plt.show()
 
